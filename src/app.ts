7b432e43a1db5e73ec7ae4eaa0d88df4dca77922
Jerneja <bozic.jerneja@gmail.com>
2023-12-11 12:49:58 +0000

    chore: add server and app separately, use express

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.use(cors())
+
+export default app
