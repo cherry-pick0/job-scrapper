4ed7d3dc1c02329865a9c4b6f6055ad20610c2ab
Jerneja <bozic.jerneja@gmail.com>
2024-01-21 19:22:02 +0000

    feat: add link to model

--- src/db/models/ScrappedJobData.ts
+++ src/db/models/ScrappedJobData.ts
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 const jobSchema = new mongoose.Schema({
   title: String,
   company: String,
-  location: String
+  location: String,
+  link: String
 })
 
 const scrappedJobDataSchema = new mongoose.Schema({
