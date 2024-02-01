3ce94a63822deb558464ba320f1e7cf7a6b84329
Jerneja <bozic.jerneja@gmail.com>
2024-02-01 15:22:44 +0000

    chore: use 3001 port

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import app from '@src/app'
 import getMongoDbClient from '@src/db/getMongoDbClient'
 import { scheduleLinkedInJobAIProcessing } from '@src/workers'
 
-const { PORT = 3000 } = process.env
+const { PORT = 3001 } = process.env
 
 app.listen(PORT, async () => {
   console.log(`Server running at http://localhost:${PORT}`)
